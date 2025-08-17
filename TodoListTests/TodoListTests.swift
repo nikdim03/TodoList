55bfb0597bd9e9a08a48436f6f04b6e683b037bc
Dmitrii Nikulin <nikdim_03@mail.ru>
2025-08-17 10:50:26 -0300

    Initial Commit

---
+++ TodoListTests/TodoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListTests.swift
+//  TodoListTests
+//
+//  Created by Dmitrii Nikulin on 8/17/25.
+//
+
+import Testing
+@testable import TodoList
+
+struct TodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
